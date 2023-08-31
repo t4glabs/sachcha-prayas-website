96172d84116676f70c561dc2c8086547d86f4027
Lily-Sneha <lakralily3@gmail.com>
2023-08-31 10:49:23 +0530

    Home Page-fixed hero image, Our Work- Image, About- Hero Image,

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
-    theme: {
-      extend: {
-        spacing: {
-          '112': '28rem',
-          '88': '22rem',
-          '104': '26rem',
-          '116': '29rem',
-          '120': '30rem'
-        }
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '112': '28rem',
+        '88': '22rem',
+        '104': '26rem',
+        '116': '29rem',
+        '120': '30rem',
+
       },
+
+      backgroundColor: {
+        'custom-green': 'green-600',
+      }
     },
-    plugins: [ ],
-  }
-  
-  
+  },
+  plugins: [],
+}
+
